f99eb4e65bed7d7cfacd024f1d9b4284fbcd8f6e
= <peter.duke.10@gmail.com>
2014-02-20 19:22:03 -0500

    delete user

--- app/views/layout.blade.php
+++ app/views/layout.blade.php
@@ -6,9 +6,6 @@
         <link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css">
         <link rel="stylesheet" type="text/css" href="{{ asset('styles/styles.css') }}">
 
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
-        <script src="//netdna.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap.min.js"></script>
-
     </head>
     <body>
 
